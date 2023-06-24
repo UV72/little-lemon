ade27a282b7cd13c020a4a898157b101a977b2c0
Yuvraj Bhadoriya <yuvrajbhadoriya20@gmail.com>
2023-06-24 09:28:19 +0530

    Onboarding implemented along with navigation

---
+++ app/src/main/java/com/uv72dev/littlelemon/NavigationComposable.kt
@@ -0,0 +1,23 @@
+package com.uv72dev.littlelemon
+
+import android.content.SharedPreferences
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun NavigationComposable(startDestination: Destinations, sharedPreferences: SharedPreferences){
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = startDestination.route){
+        composable(Home.route){
+            Home(navController, sharedPreferences)
+        }
+        composable(Onboarding.route){
+            Onboarding(navController, sharedPreferences)
+        }
+        composable(Profile.route){
+            Profile(navController, sharedPreferences)
+        }
+    }
+}
