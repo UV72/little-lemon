4f505785908e2695692e5531a4034bc391f6c4ed
Yuvraj Bhadoriya <yuvrajbhadoriya20@gmail.com>
2023-06-26 19:30:03 +0530

    Full functionality implemented with filtering with search phrase as well as categories

--- app/src/main/java/com/uv72dev/littlelemon/MainActivity.kt
+++ app/src/main/java/com/uv72dev/littlelemon/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.uv72dev.littlelemon
 
-import android.content.SharedPreferences.OnSharedPreferenceChangeListener
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,35 +8,56 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.lifecycleScope
+import androidx.room.Room
 import com.uv72dev.littlelemon.ui.theme.*
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
+    private val httpClient = HttpClient(Android) {
+        install(ContentNegotiation) {
+            json(contentType = ContentType("text", "plain"))
+        }
+    }
 
-//    private val sharedPreferences by lazy {
-//        getSharedPreferences("LittleLemon", MODE_PRIVATE)
-//    }
-//
-//    private val loggedIn = MutableLiveData<Boolean>()
-//
-//    private val prefListener = OnSharedPreferenceChangeListener{sharedPreferences, s ->
-//        loggedIn.value = sharedPreferences.getBoolean(s, true)
-//    }
-
-
+    private val database by lazy {
+        Room.databaseBuilder(applicationContext, AppDatabase::class.java, "database").build()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        var firstScreen: Destinations = Onboarding
-//        if(loggedIn){
-//            firstScreen = Home
-//        }
+
         val sharedPreferences = getSharedPreferences("LittleLemon", MODE_PRIVATE)
+        lifecycleScope.launch(Dispatchers.IO) {
+            if (database.menuItemDao().isEmpty()) {
+                saveMenuToDatabase(fetchMenu())
+            }
+        }
         setContent {
             LittleLemonTheme {
+                // add databaseMenuItems code here
+                val databaseMenuItems = database.menuItemDao().getAll().observeAsState(emptyList())
+                // add orderMenuItems variable here
+                var orderMenuItems = remember {
+                    mutableStateOf(false)
+                }
+                // add menuItems variable here
+                var menuItems = databaseMenuItems.value
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -48,10 +68,24 @@ class MainActivity : ComponentActivity() {
                     if(sharedPreferences.getBoolean("loggedIn", false)){
                         startLocation = Home
                     }
-                    NavigationComposable(startLocation, sharedPreferences)
+                    NavigationComposable(startLocation, sharedPreferences, menuItems)
                 }
             }
         }
+
+
+    }
+    private suspend fun fetchMenu(): List<MenuItemNetwork> {
+
+        val response =  httpClient
+            .get("https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json")
+            .body<MenuNetwork>()
+        return response.menu
+        // data URL: https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/littleLemonSimpleMenu.json
+    }
+    private fun saveMenuToDatabase(menuItemsNetwork: List<MenuItemNetwork>) {
+        val menuItemsRoom = menuItemsNetwork.map { it.toMenuItemRoom() }
+        database.menuItemDao().insertAll(*menuItemsRoom.toTypedArray())
     }
 }
 
